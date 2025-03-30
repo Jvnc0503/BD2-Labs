30c48908fe2cbd2a8f2b921d194144976ca6bf40
Jvnc0503 <jvnc0503@gmail.com>
2025-03-30 11:52:59 -0500

    Added Matricula, Metadata and Manager to P2.cpp

--- Lab01/P2.cpp
+++ Lab01/P2.cpp
@@ -1,4 +1,31 @@
 #include <iostream>
+#include <ios>
+#include <utility>
+#include <vector>
+
+// Registro Matrícula con tamaño variable
+struct Matricula {
+    std::string codigo;
+    int ciclo;
+    double mensualidad;
+    std::string observaciones;
+};
+
+// Metadata para cada registro
+struct Metadata {
+    size_t pos; // Posición del registro en el archivo
+    size_t size; // Tamaño del registro
+    bool active; // Indica si el registro está activo o no
+};
+
+class Manager {
+    std::string dataFilename;
+    std::string metaFilename;
+
+public:
+    Manager(std::string data, std::string meta) : dataFilename(std::move(data)), metaFilename(std::move(meta)) {
+    }
+};
 
 int main() {
     return 0;
