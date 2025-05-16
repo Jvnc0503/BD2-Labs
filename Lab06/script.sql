09ed613ef124611d1a91817726378c4c405d38cb
Jvnc0503 <jvnc0503@gmail.com>
2025-05-15 23:26:18 -0500

    Added Lab06

---
+++ Lab06/script.sql
@@ -0,0 +1,13 @@
+--plpgsql
+CREATE DATABASE Lab06;
+CREATE SCHEMA IF NOT EXISTS public;
+SET search_path TO Lab06, public;
+
+--P1
+
+
+--P2
+--Restore the database from the backup file
+SELECT *
+FROM film;
+
