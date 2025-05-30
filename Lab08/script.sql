01afa0630adb6c9d7cdcea5f32ce373e693af2dd
Jvnc0503 <jvnc0503@gmail.com>
2025-05-30 18:02:06 -0500

    commit

---
+++ Lab08/script.sql
@@ -0,0 +1,41 @@
+create extension cube;
+
+drop table if exists vectors;
+
+create table if not exists vectors
+(
+    id            serial,
+    vector_lineal cube,
+    vector_gist   cube
+);
+
+--Populate the table with random data
+insert into vectors(id, vector_lineal)
+select id,
+       cube(array [round(random() * 1000),
+           round(random() * 1000),
+           round(random() * 1000),
+           round(random() * 1000)])
+from generate_series(1, 100000) as id;
+
+update vectors
+set vector_gist = vector_lineal;
+
+--Create rtree index
+create index idx_vector on vectors using gist (vector_gist);
+
+VACUUM FULL vectors;
+
+--KNN without index
+explain analyze
+select id, vector_lineal, cube_distance(vector_lineal, '(638, 616, 153, 907)') as distance
+from vectors
+order by vector_lineal <-> '(636, 616, 153, 907)'
+limit 5;
+
+--KNN with index
+explain analyze
+select id, vector_gist, cube_distance(vector_gist, '(638, 616, 153, 907)') as distance
+from vectors
+order by vector_gist <-> '(636, 616, 153, 907)'
+limit 5;
