3c61416579dd5b0a6ef2c3631582e83ba804a0f1
Jvnc0503 <jvnc0503@gmail.com>
2025-07-08 20:04:47 -0500

    commit

--- Lab15/lab15.sql
+++ Lab15/lab15.sql
@@ -1,4 +1,11 @@
-create table if not exists sensor_readings
-(
+drop table if exists temperature_measurements;
 
+create table if not exists temperature_measurements
+(
+    sensor_id   varchar(20),
+    date        varchar(10),
+    event_time  timestamp,
+    temperature double precision,
+    humidity    double precision,
+    primary key (sensor_id, date, event_time)
 );
